eafb89079d983b4a2f5a2a59ed91230eb2951177
Ivan Bakaidov <aacidov@Air-Ivan.Dlink>
2017-11-24 16:22:19 +0300

    add schedule abstraction

---
+++ src/js/scheduler.js
@@ -0,0 +1,12 @@
+const schedule = require('node-schedule')
+
+const {time} = require('../../config')
+
+time = time.split(':')
+
+const hour = time[0]
+const minute = time[1]
+
+module.exports = function (cb){
+  schedule.scheduleJob(`${minute} ${hour} * * *`, cb)
+}
